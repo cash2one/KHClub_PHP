38d4fc1f787f52477e32c93780e50651a387583c
李晓航 <lixiaohang@lixiaohangdeMac-mini.local>
2016-05-06 18:38:26 +0800

    UI最终调整

--- BusinessServer/Application/Home/Controller/IndexController.class.php
+++ BusinessServer/Application/Home/Controller/IndexController.class.php
@@ -44,7 +44,7 @@ class IndexController extends Controller {
                       "name" : "品位环球",
                       "sub_button" : [
                         {
-                          "url" : "http://app.qq.com/#id=detail&appid=1105254023",
+                          "url" : "http://a.app.qq.com/o/simple.jsp?pkgname=com.shs.global",
                           "name" : "APP下载",
                           "type" : "view"
                         },
